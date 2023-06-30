26455c8403270ee70b33dcfb3d9ddc6ae2f8ac9f
Young Hwang <ykcul02@gmail.com>
2023-06-30 23:18:09 +0900

    fix: reflection method reactor flux 사용

---
+++ src/main/java/io/younghwang/reactive/_02/ReactorEx.java
@@ -0,0 +1,27 @@
+package io.younghwang.reactive._02;
+
+import reactor.core.publisher.Flux;
+
+public class ReactorEx {
+    public static void main(String[] args) {
+        Flux.create(s -> {
+                    s.next(1);
+                    s.next(2);
+                    s.next(3);
+                    s.complete();
+                })
+                .log()
+                .subscribe(System.out::println);
+
+        Flux.<Integer>create(s -> {
+                    s.next(1);
+                    s.next(2);
+                    s.next(3);
+                    s.complete();
+                })
+                .log()
+                .map(s -> 10 * s)
+                .log()
+                .subscribe(System.out::println);
+    }
+}
