49ca94ed06b88e0ef8bda768ae759c54ec0916d6
Young Hwang <ykcul02@gmail.com>
2023-07-03 23:55:46 +0900

    single thread 생성을 통한 실행

--- src/main/java/io/younghwang/reactive/_03/SchedulerEx.java
+++ src/main/java/io/younghwang/reactive/_03/SchedulerEx.java
@@ -5,8 +5,14 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Slf4j
 public class SchedulerEx {
+    // reactive programming 표준 scheduler를 이용한 관리
+    // 1. publishOn: publisher는 느리나 subscriber가 빠른 경우 사용
+    // 2. subscribeOn: publisher는 빠르나 subscriber가 느린 경우 사용
     public static void main(String[] args) {
         Publisher<Integer> pub = s -> {
             s.onSubscribe(new Subscription() {
@@ -27,7 +33,15 @@ public class SchedulerEx {
             });
         };
 
-        pub.subscribe(new Subscriber<Integer>() {
+        // 중간 publisher
+        Publisher<Integer> pubSubOn = s -> {
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+            executorService.execute(() -> {
+                pub.subscribe(s);
+            });
+        };
+
+        pubSubOn.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
                 log.debug("onSubscribe");
@@ -49,5 +63,6 @@ public class SchedulerEx {
                 log.debug("onComplete");
             }
         });
+        System.out.println("exit");
     }
 }
