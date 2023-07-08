48a507c9b0ccdc145eae6dbbea5e71231950dc77
Young Hwang <ykcul02@gmail.com>
2023-07-08 19:25:21 +0900

    take publisher subscriber 구현

--- src/main/java/io/younghwang/reactive/_03/IntervalEx.java
+++ src/main/java/io/younghwang/reactive/_03/IntervalEx.java
@@ -7,7 +7,6 @@ import org.reactivestreams.Subscription;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -15,22 +14,61 @@ public class IntervalEx {
     public static void main(String[] args) {
         Publisher<Integer> pub = s -> s.onSubscribe(new Subscription() {
             int no = 0;
+            boolean canceled = false;
+
+            ScheduledExecutorService executorService;
 
             @Override
             public void request(long n) {
                 ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
                 executorService.scheduleAtFixedRate(() -> {
+                    if (canceled) {
+                        executorService.shutdown();
+                        return;
+                    }
                     s.onNext(no++);
                 }, 0, 200, TimeUnit.MILLISECONDS);
             }
 
             @Override
             public void cancel() {
-
+                this.canceled = true;
             }
         });
 
-        pub.subscribe(new Subscriber<Integer>() {
+        Publisher<Integer> takePub = sub -> {
+            pub.subscribe(new Subscriber<Integer>() {
+                int count = 0;
+                Subscription subscription;
+
+                @Override
+                public void onSubscribe(Subscription s) {
+                    this.subscription = s;
+                    sub.onSubscribe(s);
+                }
+
+                @Override
+                public void onNext(Integer integer) {
+                    sub.onNext(integer);
+                    if (++count > 10) {
+                        this.subscription.cancel();
+                        sub.onComplete();
+                    }
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    sub.onError(t);
+                }
+
+                @Override
+                public void onComplete() {
+                    sub.onComplete();
+                }
+            });
+        };
+
+        takePub.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
                 log.debug("onSubscribe");
