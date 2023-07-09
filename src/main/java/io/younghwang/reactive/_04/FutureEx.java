9f474e0e5380a5567a27e42947cb5fc250e9db56
Young Hwang <ykcul02@gmail.com>
2023-07-09 19:00:31 +0900

    FutureTask 를 이용한 callback, Future 동시 사용

--- src/main/java/io/younghwang/reactive/_04/FutureEx.java
+++ src/main/java/io/younghwang/reactive/_04/FutureEx.java
@@ -2,21 +2,31 @@ package io.younghwang.reactive._04;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService es = Executors.newCachedThreadPool();
+
         Future<String> result = es.submit(() -> {
             Thread.sleep(2000);
             log.info("Hello, world");
             return "Hello, world";
         });
+
         log.info(result.get());
+
+        // FutureTask = 비동기 callback과 Future 를 포함
+        FutureTask<String> ft = new FutureTask<>(() -> {
+            Thread.sleep(2000);
+            log.info("Hello, world");
+            return "Hello, world";
+        });
+
+        es.execute(ft);
+        log.info(ft.get());
+
         log.info("Exit");
     }
 }
