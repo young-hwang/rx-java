6aabb4127840f826f8fe74e027419d5ce79b0b2d
Young Hwang <ykcul02@gmail.com>
2023-07-09 18:48:11 +0900

    Future 이용한 데이터 받기

--- src/main/java/io/younghwang/reactive/_04/FutureEx.java
+++ src/main/java/io/younghwang/reactive/_04/FutureEx.java
@@ -2,23 +2,21 @@ package io.younghwang.reactive._04;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Slf4j
 public class FutureEx {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService es = Executors.newCachedThreadPool();
-        es.execute(() -> {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        Future<String> result = es.submit(() -> {
+            Thread.sleep(2000);
             log.info("Hello, world");
+            return "Hello, world";
         });
-
+        log.info(result.get());
         log.info("Exit");
     }
 }
