997a47812842f39b5d3eb884cc69aa1d0e3a7660
Young Hwang <ykcul02@gmail.com>
2023-07-11 00:26:13 +0900

    CallbackFutureTask 생성

--- src/main/java/io/younghwang/reactive/_04/FutureEx.java
+++ src/main/java/io/younghwang/reactive/_04/FutureEx.java
@@ -2,10 +2,24 @@ package io.younghwang.reactive._04;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
 import java.util.concurrent.*;
 
 @Slf4j
 public class FutureEx {
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+        SuccessCallback sc;
+
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc);
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService es = Executors.newCachedThreadPool();
 
@@ -20,7 +34,7 @@ public class FutureEx {
         // FutureTask = 비동기 callback과 Future 를 포함
         FutureTask<String> ft = new FutureTask<>(() -> {
             Thread.sleep(2000);
-            log.info("Hello, world");
+            log.info("Hello, world - futureTask");
             return "Hello, world";
         }) {
             @Override
@@ -38,6 +52,13 @@ public class FutureEx {
         es.execute(ft);
         log.info(ft.get());
 
+        // CallbackFutureTask 활용
+        CallbackFutureTask callbackFutureTask = new CallbackFutureTask(() -> {
+            Thread.sleep(2000);
+            log.info("Hello, world - callbackFutureTask");
+            return "Hello, world";
+        }, System.out::println);
+        es.execute(callbackFutureTask);
         log.info("Exit");
     }
 }
